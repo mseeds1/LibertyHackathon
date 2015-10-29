f2df31c4ea9b699aa87fdd84114f88bec05fdc10
ciaranmul <ciaranmul@googlemail.com>
2015-10-29 07:49:01 +0000

    social button layout changes

--- index.php
+++ index.php
@@ -118,14 +118,21 @@ window.twttr = (function(d, s, id) {
 </div>
 </div>
 
-<div>
-	<div class="fb-share-button" data-href="https://apps.facebook.com/macmillancompanion" data-layout="button"></div>
+<div style="float:left">
+	<div class="column-left">
+		<div class="fb-share-button" data-href="https://apps.facebook.com/macmillancompanion" data-layout="button"></div>
+	</div>
 	
-	<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://apps.facebook.com/macmillancompanion/" data-text="Check out the Macmillan Companion Facebook app!" data-via="macmillancancer" data-size="large">Tweet</a>
-	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	<div class="column-center">
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://apps.facebook.com/macmillancompanion/" data-text="Check out the Macmillan Companion Facebook app!" data-via="macmillancancer" data-size="large">Tweet</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
 	
-	<script src="https://apis.google.com/js/platform.js"></script>
-	<div class="g-ytsubscribe" data-channel="MacmillanCancer" data-layout="full" data-count="default"></div>
+	<div class="column-right">
+		<script src="https://apis.google.com/js/platform.js"></script>
+		<div class="g-ytsubscribe" data-channel="MacmillanCancer" data-layout="full" data-count="default"></div>
+	</div>
+	
 </div>
 	
 </body>
