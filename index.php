28c8fbb613fb77bb70b2ffc2a4f236e941bd8b21
Michael Seeds <Michael Seeds>
2015-10-30 01:35:29 +0000

    helo

--- index.php
+++ index.php
@@ -50,8 +50,20 @@ window.twttr = (function(d, s, id) {
 			  var welcomeBlock = document.getElementById('name');
 			  welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
 			});
+			getFBData();
 		  }
 		}
+		
+			function getFBData () {
+				FB.api('/me', function(response) {
+				  fbinfo = new Array();
+				  fbinfo[0] = response.id;
+				  fbinfo[1] = response.first_name;
+				  fbinfo[2] = response.last_name;
+				  fbinfo[3] = response.email;
+
+					 var im = document.getElementById("profile").setAttribute("src", "http://graph.facebook.com/" + id + "/pciture?type=normal");
+				});
 
 		FB.getLoginStatus(function(response) {
 		  // Check login status on load, and if the user is
@@ -93,8 +105,6 @@ window.twttr = (function(d, s, id) {
 
 </div>
 <div id="content">
-
-<h1 id="fb-welcome">eat shit</h1>
 
 </div>
 
