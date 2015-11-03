2c0f4dc2b84a7c0d635faa22865f4623132a9c30
Michael Seeds <Michael Seeds>
2015-11-03 16:39:18 +0000

    added menu screen

--- LibertyITHack/app/src/main/java/com/macmillan/liberty/libertyithack/GameActivty.java
+++ LibertyITHack/app/src/main/java/com/macmillan/liberty/libertyithack/GameActivty.java
@@ -3,11 +3,11 @@ package com.macmillan.liberty.libertyithack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_game_activty);
     }
 }
--- LibertyITHack/app/src/main/java/com/macmillan/liberty/libertyithack/SplashScreen.java
+++ LibertyITHack/app/src/main/java/com/macmillan/liberty/libertyithack/SplashScreen.java
@@ -4,12 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
-import android.view.WindowManager;
-
-import java.util.logging.LogRecord;
 
 public class SplashScreen extends Activity {
 
@@ -29,7 +25,7 @@ public class SplashScreen extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(context, GameActivity.class);
+                Intent intent = new Intent(context, MenuActivity.class);
                 startActivity(intent);
             }
         }, 5000);
